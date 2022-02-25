2260c96ea963b532334c4e6a2c9244ed9d89f217
ivan <iromerosanchez9@gmail.com>
2022-02-25 20:00:17 +0100

    vsdbbrbrebbeebebebbebeb

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ function _id(ident){
     return document.getElementById(ident);
 }
 
-function factorial(){
-    
+function factorial() {
+    var entradaFactorial = _id("entradaFactorial").value;
+    var operacion = "";
+    var resultado = 1;
+
+    if (entradaFactorial < 0) {
+        alert('No se puede calcular');
+    } else {
+        for (var i = entradaFactorial; i > 0; i--) {
+            resultado *= i;
+            operacion += i;
+
+            if (i>1) operacion+="*";
+        } 
+        _id("resultatFactorial").innerHTML = resultado;
+        _id("operacionsFactorial").innerHTML = operacion;
+
+
+    }
+
 }
 
 function calculaIVA(){
@@ -11,5 +29,16 @@ function calculaIVA(){
 }
 
 function canviaFons(tipus){
+        var simbolos, color;
+        simbolos = "0123456789ABCDEF";
+        color = "#";
     
-}
+        for(var i = 0; i < 6; i++){
+            color = color + simbolos[Math.floor(Math.random() * 16)];
+        }
+    
+        document.body.style.background = color;
+        document.body.style.font = color;
+        document.getElementById("hexadecimal").innerHTML = color;
+        document.getElementById("text").innerHTML = "Copiar Color";
+    }
